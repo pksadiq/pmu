a9fe3a9bf60ddc4690550775361deab6d652e53e
Mohammed Sadiq <sadiq@sadiqpk.org>
2017-03-03 09:02:47 +0530

    window: consider setup-window as a dialog

--- src/pmu-window.c
+++ src/pmu-window.c
@@ -118,6 +118,9 @@ show_setup_window (GSimpleAction *action,
   window = pmu_setup_window_new (app);
   gtk_window_set_transient_for (GTK_WINDOW (window), GTK_WINDOW (self));
   gtk_window_set_modal (GTK_WINDOW (window), TRUE);
+  gtk_window_set_position (GTK_WINDOW (window), GTK_WIN_POS_CENTER_ON_PARENT);
+  gtk_window_set_type_hint (GTK_WINDOW (window), GDK_WINDOW_TYPE_HINT_DIALOG);
+
   gtk_window_present (GTK_WINDOW (window));
 }
 
