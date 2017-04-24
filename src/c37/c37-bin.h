613dd0e727b92ba7a9b8a09da1d3e4cf74b0b826
Mohammed Sadiq <sadiq@sadiqpk.org>
2017-04-24 08:39:17 +0530

    c37-bin: add API declratations to header

--- src/c37/c37-bin.h
+++ src/c37/c37-bin.h
@@ -22,6 +22,23 @@
 
 #include "c37-common.h"
 
+byte     cts_bin_get_type           (const byte *data);
+uint16_t cts_bin_get_frame_size     (const byte *data);
+uint16_t cts_bin_get_id_code        (const byte *data,
+                                     bool        skip_first);
+uint32_t cts_bin_get_time_seconds   (const byte *data,
+                                     bool        skip_first);
+uint32_t cts_bin_get_frac_of_second (const byte *data,
+                                     uint32_t    time_base,
+                                     bool        skip_first);
+uint16_t cts_bin_get_crc            (const byte *data,
+                                     const byte *header);
+
+
+uint16_t cts_bin_get_command_type (const byte *data,
+                                   bool        skip_first);
+
+
 
 #endif /* C37_BIN_H */
 
